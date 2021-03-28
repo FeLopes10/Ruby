50f12fa6b875efab8e33257e6e5828e624c24225
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2021-03-28 01:20:37 -0300

    Create metodos.rb

---
+++ metodos.rb
@@ -0,0 +1,25 @@
+# Ruby
+Ruby. Desbloqueie o medo de programar
+
+
+def diga_ola(nome)
+    puts 'Olá,' + nome
+end
+
+diga_ola('Felipe')
+
+# SELECIONAR TODAS A LINHAS QUE QUER COMITAR E APERTAR CTRL+; 
+
+#def retorna_nome
+#    'Felipe'
+#end
+
+#resultado = retorna_nome
+#puts resultado
+
+# def soma(v1, v2)
+#     total = v1 + v2
+# end
+
+# res = soma(10,25)
+# puts res
