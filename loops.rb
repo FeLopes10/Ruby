47fe6f554c0bf3d2c7653d4f31e8cf29da341cf9
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2021-03-28 01:20:08 -0300

    Create loops.rb

---
+++ loops.rb
@@ -0,0 +1,32 @@
+# Ruby
+Ruby. Desbloqueie o medo de programar
+
+
+# Repetir a mensagem na tela
+
+# 5.times do |i|
+#    puts 'Repetindo a mensagem ' +  i.to_s + ' vez(es)'
+# end
+
+# WHILE -----------
+
+# init = 0
+
+# while init <= 10 do
+#     puts 'Lopping infinito ' +  init.to_s + ' vez(es)'
+#     init += 1
+# end
+
+# FOR --------
+
+# for item in (0...10)
+#     puts 'Repetindo a mensagem ' +  item.to_s + ' vez(es)'
+# end
+
+# ARRAYS  ---- conjunto somatorio de string ou numeros identificado por []
+
+vingadores = ['Ironman', 'Hulk', 'Spiderman', 'Thor']
+
+vingadores.each do |v|
+    puts v
+end
