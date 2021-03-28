0a9c6d4ed09cebd6a1010303d4d3e835dd3c107b
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2021-03-28 01:19:00 -0300

    Create hashes.rb

---
+++ hashes.rb
@@ -0,0 +1,13 @@
+# Ruby
+Ruby. Desbloqueie o medo de programar
+
+
+# Hashes é um objeto com uma coleção de dados feita através de chave e valor
+
+carro = Hash[nome: 'Civic', marca: 'Honda', cor: 'Vermelho']
+
+puts carro[:nome]
+
+carro[:modelo] = 'SI'
+
+puts carro
