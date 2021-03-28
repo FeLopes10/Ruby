a619be234df10cdb12a073512239050cc83e97a9
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2021-03-28 01:21:21 -0300

    Create operadores.rb

---
+++ operadores.rb
@@ -0,0 +1,31 @@
+# numero1 = gets.chomp.to_f
+# numero2 = gets.chomp.to_f
+
+# total = numero1 / numero2
+
+# 2. Converter o valor de número1 e número2 pra float na divisão: 
+
+# numero1 = 5
+
+# numero2 = 2
+
+# total = numero1.to_f / numero2.to_f
+
+
+#total = numero1 / numero2
+#puts total
+
+# Operadores de Comparação
+
+# > - maior
+# < - menor
+# >= - maior ou igual
+# <= - menor ou igual
+# != - diferente
+# == - igual
+# eql? - igual
+
+v1 = 11
+v2 = 10
+
+puts v1 != v2
